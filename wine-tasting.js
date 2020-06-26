2b12b0719224086fa9b259ad68d09e35dfc1e276
Blake Hecksher <blake.hecksher@gmail.com>
2020-06-26 19:41:55 -0400

    Set up notes in containers

--- wine-tasting.js
+++ wine-tasting.js
@@ -17,6 +17,21 @@ const aromaWheel = {
 				'Hibiscus'
 			]
 		},
+		Spice         : {
+			name  : 'Spice',
+			notes : [
+				'White Pepper',
+				'Red Pepper',
+				'Black Pepper',
+				'Cinnamon',
+				'Anise',
+				'Asian 5-Spice',
+				'Fennel',
+				'Eucalyptus',
+				'Mint',
+				'Thyme'
+			]
+		},
 		Citrus        : {
 			name  : 'Citrus',
 			notes : [
@@ -89,21 +104,6 @@ const aromaWheel = {
 				'Beeswax',
 				'Ginger',
 				'Saffron'
-			]
-		},
-		Spice         : {
-			name  : 'Spice',
-			notes : [
-				'White Pepper',
-				'Red Pepper',
-				'Black Pepper',
-				'Cinnamon',
-				'Anise',
-				'Asian 5-Spice',
-				'Fennel',
-				'Eucalyptus',
-				'Mint',
-				'Thyme'
 			]
 		},
 		Vegetable     : {
@@ -218,43 +218,53 @@ const aromaWheel = {
 // Write tasting note categories
 for (const element in aromaWheel) {
 	document.getElementById('notes').innerHTML +=
-		'<ul class="firstNotes" id="' + element.toLowerCase() + '"><p>' + element + '</p></ul>';
+		'<ul class="firstNotes" id="' +
+		element.toLowerCase() +
+		'"><h3>' +
+		element +
+		'</h3><ul class="notes"></ul></ul>';
 }
 // Write Primary notes
 for (const element in aromaWheel.Primary) {
-	document.getElementById('primary').innerHTML +=
-		'<li>' +
+	document.getElementById('primary').getElementsByClassName('notes')[0].innerHTML +=
+		'<li><h4>' +
 		aromaWheel.Primary[element].name +
-		'</li><ul id="' +
+		'</h4><ul id="' +
 		aromaWheel.Primary[element].name +
 		'"><li>' +
 		aromaWheel.Primary[element].notes.join('<li>') +
-		'</li></ul>';
+		'</li></li></ul>';
 }
 // Write Secondary notes
 for (const element in aromaWheel.Secondary) {
-	document.getElementById('secondary').innerHTML +=
-		'<li>' +
+	document.getElementById('secondary').getElementsByClassName('notes')[0].innerHTML +=
+		'<li><h4>' +
 		aromaWheel.Secondary[element].name +
-		'</li><ul><li>' +
+		'</h4><ul id="' +
+		aromaWheel.Secondary[element].name +
+		'"><li>' +
 		aromaWheel.Secondary[element].notes.join('<li>') +
-		'</li></ul>';
+		'</li></li></ul>';
 }
 // Write Tertiary notes
 for (const element in aromaWheel.Tertiary) {
-	document.getElementById('tertiary').innerHTML +=
-		'<li>' +
+	document.getElementById('tertiary').getElementsByClassName('notes')[0].innerHTML +=
+		'<li><h4>' +
 		aromaWheel.Tertiary[element].name +
-		'</li><ul><li>' +
+		'</h4><ul id="' +
+		aromaWheel.Tertiary[element].name +
+		'"><li>' +
 		aromaWheel.Tertiary[element].notes.join('<li>') +
-		'</li></ul>';
+		'</li></li></ul>';
 }
 // Write Fault notes
 for (const element in aromaWheel.Faults) {
-	document.getElementById('faults').innerHTML +=
-		'<li>' +
+	document.getElementById('faults').getElementsByClassName('notes')[0].innerHTML +=
+		'<li><h4>' +
 		aromaWheel.Faults[element].name +
-		'</li><ul><li>' +
+		'</h4><ul id="' +
+		aromaWheel.Faults[element].name +
+		'"><li>' +
 		aromaWheel.Faults[element].notes.join('<li>') +
-		'</li></ul>';
+		'</li></li></ul>';
 }
