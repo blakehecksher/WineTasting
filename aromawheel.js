82980c652943311a3f4045eecf1a3c4e88bfd359
Matheck <blake.hecksher@gmail.com>
2020-06-27 14:30:44 -0400

    Added import / export functionality

---
+++ aromawheel.js
@@ -0,0 +1,217 @@
+export const aromaWheel = {
+	Primary   : {
+		Flower        : {
+			name  : 'Flower',
+			notes : [
+				'Iris',
+				'Peony',
+				'Elderflower',
+				'Accacia',
+				'Lilac',
+				'Jasmine',
+				'Honesysuckle',
+				'Violet',
+				'Lavender',
+				'Rose',
+				'Potpurri',
+				'Hibiscus'
+			]
+		},
+		Spice         : {
+			name  : 'Spice',
+			notes : [
+				'White Pepper',
+				'Red Pepper',
+				'Black Pepper',
+				'Cinnamon',
+				'Anise',
+				'Asian 5-Spice',
+				'Fennel',
+				'Eucalyptus',
+				'Mint',
+				'Thyme'
+			]
+		},
+		Citrus        : {
+			name  : 'Citrus',
+			notes : [
+				'Lime',
+				'Lemon',
+				'Grapefruit',
+				'Orange',
+				'Marmalade'
+			]
+		},
+		TreeFruit     : {
+			name  : 'Tree Fruit',
+			notes : [
+				'Quince',
+				'Apple',
+				'Pear',
+				'Nectarine',
+				'Peach',
+				'Apricot',
+				'Persimmon'
+			]
+		},
+		TropicalFruit : {
+			name  : 'Tropical Fruit',
+			notes : [
+				'Pineapple',
+				'Mango',
+				'Guava',
+				'Kiwi',
+				'Lychee',
+				'Bubblegum'
+			]
+		},
+		RedFruit      : {
+			name  : 'Red Fruit',
+			notes : [
+				'Cranberry',
+				'Red Plum',
+				'Pomegranate',
+				'Sour Cherry',
+				'Strawberry',
+				'Cherry',
+				'Raspberry'
+			]
+		},
+		BlackFruit    : {
+			name  : 'Black Fruit',
+			notes : [
+				'Boysenberry',
+				'Black Currant',
+				'Black Cherry',
+				'Plum',
+				'Blackberry',
+				'Blueberry',
+				'Olive'
+			]
+		},
+		DriedFruit    : {
+			name  : 'Dried Fruit',
+			notes : [
+				'Raisin',
+				'Fig',
+				'Date',
+				'Fruitcake'
+			]
+		},
+		NobleRot      : {
+			name  : 'Noble Rot',
+			notes : [
+				'Beeswax',
+				'Ginger',
+				'Saffron'
+			]
+		},
+		Vegetable     : {
+			name  : 'Vegetable',
+			notes : [
+				'Grass',
+				'Tomato Leaf',
+				'Gooseberry',
+				'Bell Pepper',
+				'Jalapeno',
+				'Green Almond',
+				'Tomato',
+				'Sun-Dried Tomato',
+				'Black Tea'
+			]
+		},
+		Earth         : {
+			name  : 'Earth',
+			notes : [
+				'Clay Pot',
+				'Slate',
+				'Wet Gravel',
+				'Potting Soil',
+				'Red Beet',
+				'Volcanic Rock',
+				'Petroleum'
+			]
+		}
+	},
+	Secondary : {
+		Microbial : {
+			name  : 'Microbial',
+			notes : [
+				'Mushroom',
+				'Truffle',
+				'Lager',
+				'Sourdough',
+				'Cream',
+				'Butter'
+			]
+		}
+	},
+	Tertiary  : {
+		OakAging     : {
+			name  : 'Oak Aging',
+			notes : [
+				'Dill',
+				'Smoke',
+				'Cigar Box',
+				'Baking Spices',
+				'Coconut',
+				'Vanilla'
+			]
+		},
+		GeneralAging : {
+			name  : 'General Aging',
+			notes : [
+				'Leather',
+				'Cocoa',
+				'Coffee',
+				'Tobacco',
+				'Nuts',
+				'Dried Fruit'
+			]
+		}
+	},
+	Faults    : {
+		TCA                : {
+			name  : 'TCA',
+			notes : [
+				'Wet Dog',
+				'Musty Cardboard'
+			]
+		},
+		SulfidesMercaptans : {
+			name  : 'Sulfides & Mercaptans',
+			notes : [
+				'Cat Pee',
+				'Onion',
+				'Garlic',
+				'Match Box',
+				'Burnt Rubber',
+				'Boiled Eggs',
+				'Cured Meat'
+			]
+		},
+		Brettanomyces      : {
+			name  : 'Brettanomyces',
+			notes : [
+				'Horse Manure',
+				'Sweaty Leather Saddle',
+				'Band-Aid',
+				'Black Cardomom'
+			]
+		},
+		Cooked             : {
+			name  : 'Cooked',
+			notes : [
+				'Stewed Fruit',
+				'Toffee'
+			]
+		},
+		VolatileAcidity    : {
+			name  : 'Volatile Acidity',
+			notes : [
+				'Balsamic',
+				'Vinegar'
+			]
+		}
+	}
+}
